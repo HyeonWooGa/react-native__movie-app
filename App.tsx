f28e63318af071a08117b25ed955c84c427fe5aa
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-10 21:36:35 +0900

    🚧 AppLoading 'Hooks ' 사용 전

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
-import { Text } from "react-native";
+import { Image, Text } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
-  const startLoading = async () => {};
+  const startLoading = async () => {
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require("./photo.jpeg"));
+    await Image.prefetch(
+      "https://d33wubrfki0l68.cloudfront.net/4245a6b338cc1b008aa1265c213c1e75be207801/2eaf7/img/oss_logo.svg"
+    );
+  };
   if (!ready) {
     return (
       <AppLoading
