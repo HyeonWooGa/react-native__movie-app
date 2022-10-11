841d81dbe02c7399f0ff3f46badc9444021827f8
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-11 21:55:34 +0900

    🚧 Tab Navigator 다크모드 적용

--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -3,26 +3,29 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movie from "../screens/Movie";
 import Search from "../screens/Search";
 import Tv from "../screens/Tv";
-import { Text, View } from "react-native";
+import { useColorScheme } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
+  const isDark = useColorScheme() === "dark";
+
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarStyle: { backgroundColor: "tomato" },
-        tabBarActiveTintColor: "red",
-        tabBarInactiveTintColor: "purple",
-        headerTitleStyle: { color: "tomato" },
-        headerRight: () => (
-          <View>
-            <Text>Hello</Text>
-          </View>
-        ),
+        tabBarStyle: {
+          backgroundColor: isDark ? "black" : "white",
+        },
+        tabBarActiveTintColor: isDark ? "white" : "black",
+        headerStyle: {
+          backgroundColor: isDark ? "black" : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : "black",
+        },
       }}
     >
-      <Tab.Screen name="Movies" component={Movie} />
+      <Tab.Screen name="Movie" component={Movie} />
       <Tab.Screen name="Tv" component={Tv} />
       <Tab.Screen name="Search" component={Search} />
     </Tab.Navigator>
