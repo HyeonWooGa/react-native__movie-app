abbec3f1e379ab91e4b2c494a3bd16e7fa2ea561
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-11 21:41:27 +0900

    🚧 Tab Navigator ScreenOptions 속성

--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -3,13 +3,26 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movie from "../screens/Movie";
 import Search from "../screens/Search";
 import Tv from "../screens/Tv";
+import { Text, View } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Movie" component={Movie} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { backgroundColor: "tomato" },
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "purple",
+        headerTitleStyle: { color: "tomato" },
+        headerRight: () => (
+          <View>
+            <Text>Hello</Text>
+          </View>
+        ),
+      }}
+    >
+      <Tab.Screen name="Movies" component={Movie} />
       <Tab.Screen name="Tv" component={Tv} />
       <Tab.Screen name="Search" component={Search} />
     </Tab.Navigator>
