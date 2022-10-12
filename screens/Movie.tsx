a1ccb483b59da05f162221983f59fb8e33fe4433
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-13 02:28:41 +0900

    🔥 Movie 스크린 기존 코드 삭제

--- screens/Movie.tsx
+++ screens/Movie.tsx
@@ -1,29 +1,6 @@
 import React from "react";
-import { Text, Pressable } from "react-native";
 import styled from "styled-components/native";
 
-const Btn = styled.Pressable`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.mainBgColor};
-`;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
-`;
-
-export default function Movie({ navigation: { navigate } }: any) {
-  return (
-    <Btn
-      onPress={() => navigate("Stack", { screen: "Three" })}
-      style={({ pressed }) => [
-        {
-          opacity: pressed ? 0.3 : 1.0,
-        },
-      ]}
-    >
-      <Title>Movie</Title>
-    </Btn>
-  );
+export default function Movie() {
+  return;
 }
