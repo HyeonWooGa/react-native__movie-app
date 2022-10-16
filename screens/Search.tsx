08a38ef816db2260398ff91ae927410b604b80b7
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-17 02:30:45 +0900

    ✨onChangeText 함수 생성

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
 
 const Container = styled.ScrollView``;
@@ -14,9 +14,18 @@ const SearchBar = styled.TextInput.attrs((props) => ({
 `;
 
 export default function Search() {
+  const [query, setQuery] = useState("");
+  const onChangeText = (text: string) => {
+    setQuery(text);
+  };
+  // console.log(query); // onChangeText 함수 안에 있으면 제대로 적용 안됨
   return (
     <Container>
-      <SearchBar placeholder="Search for Movie or TV Show" />
+      <SearchBar
+        placeholder="Search for Movie or TV Show"
+        returnKeyType="search"
+        onChangeText={onChangeText}
+      />
     </Container>
   );
 }
