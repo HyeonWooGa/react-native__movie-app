38a04c41f831af2e4999f1dc81e64398277fcf7b
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-17 01:30:41 +0900

    ✨TV 탭 렌더링

--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,10 +1,48 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
-import { View, Text } from "react-native";
+import { ScrollView, RefreshControl } from "react-native";
+import { tvApi } from "../api";
+import HList from "../components/HList";
+import Loader from "../components/Loader";
 
 export default function Tv() {
+  const queryClient = useQueryClient();
+  const {
+    data: todayData,
+    isLoading: isLoadingToday,
+    isRefetching: isRefetchingToday,
+  } = useQuery(["tv", "today"], tvApi.airingToday);
+  const {
+    data: topData,
+    isLoading: isLoadingTop,
+    isRefetching: isRefetchingTop,
+  } = useQuery(["tv", "top"], tvApi.topRated);
+  const {
+    data: trendingData,
+    isLoading: isLoadingTrending,
+    isRefetching: isRefetchingTrending,
+  } = useQuery(["tv", "trending"], tvApi.trending);
+  const onRefresh = () => {
+    queryClient.refetchQueries(["tv"]);
+  };
+
+  const loading = isLoadingToday || isLoadingTop || isLoadingTrending;
+  const refreshing = isRefetchingToday || isRefetchingTop || isLoadingTrending;
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Tv</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+      contentContainerStyle={{ paddingVertical: 30 }}
+    >
+      <HList title="Trending TV" data={trendingData.results} />
+      <HList title="Airing Today" data={todayData.results} />
+      <HList title="Top Rated TV" data={topData.results} />
+    </ScrollView>
   );
 }
