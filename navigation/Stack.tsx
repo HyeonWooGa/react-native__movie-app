a1e1a24789e17d51b9d1a018f079089581671c83
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-17 22:25:20 +0900

    🔥 Stack Navigation 임시 Screen 들 삭제

--- navigation/Stack.tsx
+++ navigation/Stack.tsx
@@ -1,31 +1,8 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Pressable, Text } from "react-native";
-
-const ScreenOne = ({ navigation: { navigate } }: any) => (
-  <Pressable onPress={() => navigate("Two")}>
-    <Text>Go to Two</Text>
-  </Pressable>
-);
-const ScreenTwo = ({ navigation: { navigate } }: any) => (
-  <Pressable onPress={() => navigate("Three")}>
-    <Text>Go To Three</Text>
-  </Pressable>
-);
-const ScreenThree = ({ navigation: { navigate } }: any) => (
-  <Pressable onPress={() => navigate("Tabs", { screen: "Search" })}>
-    <Text>Go to Search</Text>
-  </Pressable>
-);
 
 const NativeStack = createNativeStackNavigator();
 
 export default function Stack() {
-  return (
-    <NativeStack.Navigator screenOptions={{ headerTintColor: "green" }}>
-      <NativeStack.Screen name="One" component={ScreenOne} />
-      <NativeStack.Screen name="Two" component={ScreenTwo} />
-      <NativeStack.Screen name="Three" component={ScreenThree} />
-    </NativeStack.Navigator>
-  );
+  return <NativeStack.Navigator></NativeStack.Navigator>;
 }
