9edfde9061cc814941c5e53ca63342c505dbe31e
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-18 01:23:29 +0900

    🎨 Detail 탭 Type 정의

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,12 +1,22 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import styled from "styled-components/native";
+
+type RootStackParamList = {
+  Detail: { original_title: string };
+};
+
+type DetailScreenProps = NativeStackScreenProps<RootStackParamList, "Detail">;
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
 
-const Detail = ({ navigation: { setOptions }, route: { params } }) => {
+const Detail: React.FC<DetailScreenProps> = ({
+  navigation: { setOptions },
+  route: { params },
+}) => {
   useEffect(() => {
     setOptions({
       title: params.original_title ?? "No Title",
